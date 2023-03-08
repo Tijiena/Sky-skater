75feea0d04da555957244780accadf6accda638c
Tijiena <a13816106692@163.com>
2023-03-08 13:50:26 -0500

    second flying script

---
+++ Sky Skater/Assets/scripts/flying1.cs
@@ -0,0 +1,26 @@
+ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class flying1 : MonoBehaviour
+{
+    public float FlySpeed = 5;
+    public float YawAmount = 120;
+    private float Yaw;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += transform.forward *FlySpeed* Time.deltaTime;
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        Yaw += horizontalInput * YawAmount * Time.deltaTime;
+        float pitch = Mathf.Lerp(0, 20, Mathf.Abs(verticalInput)) * Mathf.Sign(verticalInput);
+        transform.localRotation = Quaternion.Euler(Vector3.up * Yaw+Vector3.right*pitch);
+    }
+}

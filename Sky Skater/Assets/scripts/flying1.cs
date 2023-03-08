84434c6c76ff472d3c3a7329b485b1094b9a05f8
Tijiena <a13816106692@163.com>
2023-03-08 15:19:53 -0500

    ,

--- Sky Skater/Assets/scripts/flying1.cs
+++ Sky Skater/Assets/scripts/flying1.cs
@@ -21,8 +21,6 @@ public class flying1 : MonoBehaviour
         float verticalInput = Input.GetAxis("Vertical");
         Yaw += horizontalInput * YawAmount * Time.deltaTime;
         float pitch = Mathf.Lerp(0, 20, Mathf.Abs(verticalInput)) * Mathf.Sign(verticalInput);
-        float roll=Mathf.Lerp(0, 30, Mathf.Abs(horizontalInput)) * -Mathf.Sign(horizontalInput);
-        transform.localRotation = Quaternion.Euler(Vector3.up * Yaw+Vector3.right*pitch+Vector3.forward*roll);
+        transform.localRotation = Quaternion.Euler(Vector3.up * Yaw+Vector3.right*pitch);
     }
 }
- 

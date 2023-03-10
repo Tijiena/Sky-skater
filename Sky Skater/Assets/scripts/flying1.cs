6e69e3678ff0bb88050aadf4213978ea9b873f90
Tijiena <a13816106692@163.com>
2023-03-10 09:48:06 -0500

    accerealte code

--- Sky Skater/Assets/scripts/flying1.cs
+++ Sky Skater/Assets/scripts/flying1.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class flying1 : MonoBehaviour
 {
-    public float FlySpeed = 5;
+    public float FlySpeed = 20;
     public float YawAmount = 120;
     private float Yaw;
     // Start is called before the first frame update
@@ -23,5 +23,10 @@ public class flying1 : MonoBehaviour
         float pitch = Mathf.Lerp(0, 20, Mathf.Abs(verticalInput)) * Mathf.Sign(verticalInput);
         float roll=Mathf.Lerp(0,30,Mathf.Abs(horizontalInput)) * -Mathf.Sign(horizontalInput);
         transform.localRotation = Quaternion.Euler(Vector3.up * Yaw+Vector3.right*pitch+Vector3.forward*roll);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        { FlySpeed = 40; }
+        if (Input.GetKeyUp(KeyCode.Space))
+        { FlySpeed = 20; }
     }
 }

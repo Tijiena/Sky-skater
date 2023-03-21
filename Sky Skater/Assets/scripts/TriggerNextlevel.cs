0828e625013c485b0332e931cfbd88c14580af90
gwell09 <wellg815@newschool.edu>
2023-03-21 13:38:54 -0400

    crimes

---
+++ Sky Skater/Assets/scripts/TriggerNextlevel.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TriggerNextlevel : MonoBehaviour
+{
+    public GameObject UiObject;
+    public GameObject cube;
+
+    void Start()
+    {
+        UiObject.SetActive(false);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            UiObject.SetActive(true);
+        }
+    }
+    void Update()
+    {
+
+    }
+
+    //void OnTriggerExit(Collider other)
+    //{
+    //    UiObject.SetActive(false);
+
+    //}
+
+}
